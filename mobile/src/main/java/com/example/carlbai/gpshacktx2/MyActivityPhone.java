d8da3d4959580165571ac53e71f02ef746f25fdb
baibye <carlbai@gmail.com>
2014-10-19 01:00:38 -0500

    Launches Navigation

--- mobile/src/main/java/com/example/carlbai/gpshacktx2/MyActivityPhone.java
+++ mobile/src/main/java/com/example/carlbai/gpshacktx2/MyActivityPhone.java
@@ -1,7 +1,9 @@
 package com.example.carlbai.gpshacktx2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -41,11 +43,17 @@ public class MyActivityPhone extends Activity implements GooglePlayServicesClien
 
     public void buttonClicked(View v) {
         if(v.getId() == R.id.btnLastLoc) {
+            String temp = "";
             Log.v("Hello", "buttonClicked");
             if(locationclient!=null && locationclient.isConnected()) {
                 Location loc =locationclient.getLastLocation();
                 Log.v("Hello", "Last Known Location :" + loc.getLatitude() + "," + loc.getLongitude());
+                temp = Double.toString(loc.getLatitude()) + ","  + Double.toString(loc.getLongitude() - .0005);
+                //temp = "30.291217, -97.734133";
             }
+            Intent i = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("google.navigation:q=" + temp));
+            startActivity(i);
         }
     }
 
