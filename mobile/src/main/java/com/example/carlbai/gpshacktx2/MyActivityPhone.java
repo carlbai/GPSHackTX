c25b6d5c48fa51d133815ea44ecdcc8f14d629fd
baibye <carlbai@gmail.com>
2014-10-18 22:31:39 -0500

    Initial commit

---
+++ mobile/src/main/java/com/example/carlbai/gpshacktx2/MyActivityPhone.java
@@ -0,0 +1,85 @@
+package com.example.carlbai.gpshacktx2;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+
+public class MyActivityPhone extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+
+
+    private LocationClient locationclient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_activity_phone);
+
+        int resp = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if(resp == ConnectionResult.SUCCESS){
+            locationclient = new LocationClient(this,this,this);
+            locationclient.connect();
+        }
+        else
+        {
+            Toast.makeText(this, "Google Play Service Error " + resp, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    public void buttonClicked(View v) {
+        if(v.getId() == R.id.btnLastLoc) {
+            Log.v("Hello", "buttonClicked");
+            if(locationclient!=null && locationclient.isConnected()) {
+                Location loc =locationclient.getLastLocation();
+                Log.v("Hello", "Last Known Location :" + loc.getLatitude() + "," + loc.getLongitude());
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_activity_phone, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
---
+++ wear/src/main/java/com/example/carlbai/gpshacktx2/MyActivityWatch.java
@@ -0,0 +1,56 @@
+package com.example.carlbai.gpshacktx2;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+
+public class MyActivityWatch extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+
+    private TextView mTextView;
+    private LocationClient locationclient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_activity_watch);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
